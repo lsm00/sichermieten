2f7bc9a61aaecb565cca041aabe33ba70fb52c56
lsm00 <mctwist98@gmx.de>
2019-01-22 20:45:45 +0100

    funktioniert

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,64 @@
+class PinsController < ApplicationController
+  # before_action :authenticate_user!
+  before_action :find_pin, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @pins = Pin.all.order("created_at DESC")
+  end
+
+  def new
+    @pin = current_user.pins.build
+  end
+
+  def show
+  end
+
+  def create
+    @pin = current_user.pins.build (pin_params)
+    if @pin.save
+      redirect_to @pin, notice: "Success creating your Pin!"
+    else
+      render 'new'
+    end
+  end
+
+
+  def edit
+    if current_user == @pin.user   #checks if current user is owner of the pin
+      @pin = Pin.find(params[:id])
+    else
+      flash[:danger] = "Wrong user! You are not allowed to do this!"
+      redirect_to @pin
+    end
+  end
+
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: "Update successful!"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if current_user == @pin.user
+      @pin.destroy
+      redirect_to root_path
+    else
+      flash[:danger] = "Wrong user!"
+      redirect_to @pin
+    end
+  end
+
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description, :image)
+  end
+
+  def find_pin
+    @pin = Pin.find(params[:id])
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_22_172355) do
+ActiveRecord::Schema.define(version: 2019_01_22_193618) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pins", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_pins_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
